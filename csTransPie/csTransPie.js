389a4ffddf49efc1092bb11119ce1bfe63fc9624
Petar Koretic <petar.koretic@gmail.com>
2012-01-30 23:34:47 +0100

    new version

--- csTransPie/csTransPie.js
+++ csTransPie/csTransPie.js
@@ -1,1 +1,265 @@
-(function(a){var b={preloadImg:!0},c=!1,d=function(a){a=a.replace(/^url\((.*)\)/,"$1").replace(/^\"(.*)\"$/,"$1");var b=new Image;b.src=a.replace(/\.([a-zA-Z]*)$/,"-hover.$1");var c=new Image;c.src=a.replace(/\.([a-zA-Z]*)$/,"-focus.$1")},e=function(b){var c=a(b.get(0).form),d=b.next();if(!d.is("label")){d=b.prev();if(d.is("label")){var e=b.attr("id");e&&(d=c.find('label[for="'+e+'"]'))}}return d.is("label")?d.css("cursor","pointer"):!1},f=function(b){var c=a(".cTPSelectWrapper ul:visible");c.each(function(){var c=a(this).parents(".cTPSelectWrapper:first").find("select").get(0);(!b||!c.oLabel||c.oLabel.get(0)!=b.get(0))&&a(this).hide()})},g=function(b){a(b.target).parents(".cTPSelectWrapper").length===0&&f(a(b.target))},h=function(){a(document).mousedown(g)},i=function(b){var c;a(".cTPSelectWrapper select",b).each(function(){c=this.selectedIndex<0?0:this.selectedIndex,a("ul",a(this).parent()).each(function(){a("a:eq("+c+")",this).click()})}),a("a.cTPCheckbox, a.cTPRadio",b).removeClass("cTPChecked"),a("input:checkbox",b).each(function(){this.checked&&a("a",a(this).parent()).addClass("cTPChecked")}),a("input:radio",b).each(function(){this.checked&&a("a",a(this).parent()).addClass("cTPCheckedR")})};a.fn.jqTransCheckBox=function(){return this.each(function(){if(a(this).hasClass("cTPHidden"))return;var b=a(this),c=this;oLabel=e(b),oLabel&&oLabel.click(function(){d.trigger("click")});var d=a('<a href="#" class="cTPCheckbox"></a>');b.addClass("cTPHidden").wrap('<span class="cTPCheckboxWrapper"></span>').parent().prepend(d),b.change(function(){return c.checked&&d.addClass("cTPChecked")||d.removeClass("cTPChecked"),!0}),d.click(function(){return b.attr("disabled")?!1:(b.trigger("click").trigger("change"),!1)}),c.checked&&d.addClass("cTPChecked")})},a.fn.jqTransRadio=function(){return this.each(function(){if(a(this).hasClass("cTPHidden"))return;var b=a(this),c=this;oLabel=e(b),oLabel&&oLabel.click(function(){d.trigger("click")});var d=a('<a href="#" class="cTPRadio" rel="'+this.name+'"></a>');b.addClass("cTPHidden").wrap('<span class="cTPRadioWrapper"></span>').parent().prepend(d),b.change(function(){return c.checked&&d.addClass("cTPCheckedR")||d.removeClass("cTPCheckedR"),!0}),d.click(function(){return b.attr("disabled")?!1:(b.trigger("click").trigger("change"),a('input[name="'+b.attr("name")+'"]',c.form).not(b).each(function(){a(this).attr("type")=="radio"&&a(this).trigger("change")}),!1)}),c.checked&&d.addClass("cTPCheckedR")})},a.fn.jqTransSelect=function(){return this.each(function(b){var c=a(this);if(c.hasClass("cTPHidden"))return;if(c.attr("multiple"))return;var d=e(c),g=c.addClass("cTPHidden").wrap('<div class="cTPSelectWrapper"></div>').parent().css({zIndex:100-b});g.prepend('<div><span></span><a href="#" class="cTPSelectOpen"></a></div><ul></ul>');var h=a("ul",g).css("width",c.width()).hide();a("option",this).each(function(b){var c=a('<li><a href="#" index="'+b+'">'+a(this).html()+"</a></li>");h.append(c)}),h.find("a").click(function(){return a("a.selected",g).removeClass("selected"),a(this).addClass("selected"),c[0].selectedIndex!=a(this).attr("index")&&(c[0].selectedIndex=a(this).attr("index"),a(c[0]).trigger("change")),c[0].selectedIndex=a(this).attr("index"),a("span:eq(0)",g).html(a(this).html()),h.hide(),!1}),a("a:eq("+this.selectedIndex+")",h).click(),a("span:first",g).click(function(){a("a.cTPSelectOpen",g).trigger("click")}),d&&d.click(function(){a("a.cTPSelectOpen",g).trigger("click")}),this.oLabel=d;var i=a("a.cTPSelectOpen",g).click(function(){return h.css("display")=="none"&&f(),c.attr("disabled")?!1:(h.slideToggle("fast",function(){var b=a("a.selected",h).offset().top-h.offset().top;h.animate({scrollTop:b})}),!1)}),j=c.outerWidth()+20,k=a("span:first",g),l=j>k.innerWidth()?j+i.outerWidth():g.width();g.css("width",l),h.css("width",l-2),k.css({width:j}),h.css({display:"block",visibility:"hidden"});var m=a("li",h).length*a("li:first",h).height();m<h.height()&&h.css({height:m,overflow:"hidden"}),h.css({display:"none",visibility:"visible"})})},a.fn.cTP=function(c){var d=a.extend({},b,c);return this.each(function(){a("input:checkbox",this).jqTransCheckBox(),a("input:radio",this).jqTransRadio(),a("select",this).jqTransSelect().length>0&&h(),a(this).bind("reset",function(){var a=function(){i(this)};window.setTimeout(a,10)})})}})(jQuery),$(function(){$(".transpie").cTP()})
+/*
+   based on csTransform http://www.dfc-e.com/metiers/multimedia/opensource/csTransform/
+   
+ * 30.01.2012 Petar Koretiæ
+ * Fixed few bugs, removed input, buttons and textarea functions
+ * no more form dependant - can be used on only one element, specified only on one div etc..
+ * much smaller - 4.4kb minified*
+ ******************************************** */
+ 
+ //Load css dynamically - depends on folder name - csTransPie by default
+/*
+//<![CDATA[
+if(document.createStyleSheet) { document.createStyleSheet('csTransPie/csTransPie.css'); }
+else {
+  var newSS=document.createElement('link');
+  newSS.rel='stylesheet';
+  newSS.href='csTransPie/csTransPie.css';
+  document.getElementsByTagName("head")[0].appendChild(newSS);
+}
+//]]>
+ */
+
+ 
+(function($){
+	var defaultOptions = {preloadImg:true};
+	var cTPImgPreloaded = false;
+
+	var cTPPreloadHoverFocusImg = function(strImgUrl) {
+		//guillemets to remove for ie
+		strImgUrl = strImgUrl.replace(/^url\((.*)\)/,'$1').replace(/^\"(.*)\"$/,'$1');
+		var imgHover = new Image();
+		imgHover.src = strImgUrl.replace(/\.([a-zA-Z]*)$/,'-hover.$1');
+		var imgFocus = new Image();
+		imgFocus.src = strImgUrl.replace(/\.([a-zA-Z]*)$/,'-focus.$1');				
+	};
+
+	
+	/***************************
+	  Labels
+	***************************/
+	var cTPGetLabel = function(objfield){
+		var selfForm = $(objfield.get(0).form);
+		var oLabel = objfield.next();
+		if(!oLabel.is('label')) {
+			oLabel = objfield.prev();
+			if(oLabel.is('label')){
+				var inputname = objfield.attr('id');
+				if(inputname){
+					oLabel = selfForm.find('label[for="'+inputname+'"]');
+				} 
+			}
+		}
+		if(oLabel.is('label')){return oLabel.css('cursor','pointer');}
+		return false;
+	};
+	
+	/* Hide all open selects */
+	var cTPHideSelect = function(oTarget){
+		var ulVisible = $('.cTPSelectWrapper ul:visible');
+		ulVisible.each(function(){
+			var oSelect = $(this).parents(".cTPSelectWrapper:first").find("select").get(0);
+			//do not hide if click on the label object associated to the select
+			if( !(oTarget && oSelect.oLabel && oSelect.oLabel.get(0) == oTarget.get(0)) ){$(this).hide();}
+		});
+	};
+	/* Check for an external click */
+	var cTPCheckExternalClick = function(event) {
+		if ($(event.target).parents('.cTPSelectWrapper').length === 0) { cTPHideSelect($(event.target)); }
+	};
+
+	/* Apply document listener */
+	var cTPAddDocumentListener = function (){
+		$(document).mousedown(cTPCheckExternalClick);
+	};	
+			
+	/* Add a new handler for the reset action */
+	var cTPReset = function(f){
+		var sel;
+		$('.cTPSelectWrapper select', f).each(function(){sel = (this.selectedIndex<0) ? 0 : this.selectedIndex; $('ul', $(this).parent()).each(function(){$('a:eq('+ sel +')', this).click();});});
+		$('a.cTPCheckbox, a.cTPRadio', f).removeClass('cTPChecked');
+		$('input:checkbox', f).each(function(){if(this.checked){$('a', $(this).parent()).addClass('cTPChecked');}});
+		$('input:radio', f).each(function(){if(this.checked){$('a', $(this).parent()).addClass('cTPCheckedR');}});
+		
+	};
+
+
+
+	/***************************
+	  Check Boxes 
+	 ***************************/	
+	$.fn.jqTransCheckBox = function(){
+		return this.each(function(){
+			if($(this).hasClass('cTPHidden')) {return;}
+
+			var $input = $(this);
+			var inputSelf = this;
+
+			//set the click on the label
+			oLabel=cTPGetLabel($input);
+			oLabel && oLabel.click(function(){aLink.trigger('click');});
+			
+			
+			var aLink = $('<a href="#" class="cTPCheckbox"></a>');
+			
+			$input.addClass('cTPHidden').wrap('<span class="cTPCheckboxWrapper"></span>').parent().prepend(aLink); //wrap and add the link
+			//on change, change the class of the link
+			$input.change(function(){
+				inputSelf.checked && aLink.addClass('cTPChecked') || aLink.removeClass('cTPChecked');
+				return true;
+			});
+			// Click Handler, trigger the click and change event on the input
+			aLink.click(function(){
+
+				//do nothing if the original input is disabled
+				if($input.attr('disabled')){return false;}
+				//trigger the envents on the input object
+				$input.trigger('click').trigger('change');	
+				return false;
+			});
+
+			// set the default state
+			inputSelf.checked && aLink.addClass('cTPChecked');		
+		});
+	};
+	/***************************
+	  Radio Buttons 
+	 ***************************/	
+	$.fn.jqTransRadio = function(){
+		return this.each(function(){
+			if($(this).hasClass('cTPHidden')) {return;}
+
+			var $input = $(this);
+			var inputSelf = this;
+			
+				
+			oLabel = cTPGetLabel($input);
+			oLabel && oLabel.click(function(){aLink.trigger('click');});
+	
+			var aLink = $('<a href="#" class="cTPRadio" rel="'+ this.name +'"></a>');
+			
+			$input.addClass('cTPHidden').wrap('<span class="cTPRadioWrapper"></span>').parent().prepend(aLink);
+			
+			$input.change(function(){
+				inputSelf.checked && aLink.addClass('cTPCheckedR') || aLink.removeClass('cTPCheckedR');
+				return true;
+			});
+			// Click Handler
+			aLink.click(function(){
+				if($input.attr('disabled')){return false;}
+				$input.trigger('click').trigger('change');
+	
+				// uncheck all others of same name input radio elements
+				$('input[name="'+$input.attr('name')+'"]',inputSelf.form).not($input).each(function(){
+					$(this).attr('type')=='radio' && $(this).trigger('change');
+				});
+	
+				return false;					
+			});
+			// set the default state
+			inputSelf.checked && aLink.addClass('cTPCheckedR');
+		});
+	};
+	
+
+	
+	/***************************
+	  Select 
+	 ***************************/	
+	$.fn.jqTransSelect = function(){
+		return this.each(function(index){
+			var $select = $(this);
+
+			if($select.hasClass('cTPHidden')) {return;}
+			if($select.attr('multiple')) {return;}
+
+			var oLabel  =  cTPGetLabel($select);
+			/* First thing we do is Wrap it */
+			var $wrapper = $select
+				.addClass('cTPHidden')
+				.wrap('<div class="cTPSelectWrapper"></div>')
+				.parent()
+				.css({zIndex: 100-index})
+			;
+			
+			/* Now add the html for the select */
+			$wrapper.prepend('<div><span></span><a href="#" class="cTPSelectOpen"></a></div><ul></ul>');
+			var $ul = $('ul', $wrapper).css('width',$select.width()).hide();
+			/* Now we add the options */
+			$('option', this).each(function(i){
+				var oLi = $('<li><a href="#" index="'+ i +'">'+ $(this).html() +'</a></li>');
+				$ul.append(oLi);
+			});
+			
+			/* Add click handler to the a */
+			$ul.find('a').click(function(){
+					$('a.selected', $wrapper).removeClass('selected');
+					$(this).addClass('selected');	
+					/* Fire the onchange event */
+					if ($select[0].selectedIndex != $(this).attr('index')) {
+     $select[0].selectedIndex = $(this).attr('index');
+     $($select[0]).trigger('change');
+}
+					$select[0].selectedIndex = $(this).attr('index');
+					$('span:eq(0)', $wrapper).html($(this).html());
+					$ul.hide();
+					return false;
+			});
+			/* Set the default */
+			$('a:eq('+ this.selectedIndex +')', $ul).click();
+			$('span:first', $wrapper).click(function(){$("a.cTPSelectOpen",$wrapper).trigger('click');});
+			oLabel && oLabel.click(function(){$("a.cTPSelectOpen",$wrapper).trigger('click');});
+			this.oLabel = oLabel;
+			
+			/* Apply the click handler to the Open */
+			var oLinkOpen = $('a.cTPSelectOpen', $wrapper)
+				.click(function(){
+					//Check if box is already open to still allow toggle, but close all other selects
+					if( $ul.css('display') == 'none' ) {cTPHideSelect();} 
+					if($select.attr('disabled')){return false;}
+
+					$ul.slideToggle('fast', function(){					
+						var offSet = ($('a.selected', $ul).offset().top - $ul.offset().top);
+						$ul.animate({scrollTop: offSet});
+					});
+					return false;
+				})
+			;
+
+			// Set the new width
+			var iSelectWidth = $select.outerWidth()+20;
+			var oSpan = $('span:first',$wrapper);
+			var newWidth = (iSelectWidth > oSpan.innerWidth())?iSelectWidth+oLinkOpen.outerWidth():$wrapper.width();
+			$wrapper.css('width',newWidth);
+			$ul.css('width',newWidth-2);
+			oSpan.css({width:iSelectWidth});
+		
+			// Calculate the height if necessary, less elements that the default height
+			//show the ul to calculate the block, if ul is not displayed li height value is 0
+			$ul.css({display:'block',visibility:'hidden'});
+			var iSelectHeight = ($('li',$ul).length)*($('li:first',$ul).height());//+1 else bug ff
+			(iSelectHeight < $ul.height()) && $ul.css({height:iSelectHeight,'overflow':'hidden'});//hidden else bug with ff
+			$ul.css({display:'none',visibility:'visible'});
+			
+		});
+	};
+	$.fn.cTP = function(options){
+		var opt = $.extend({},defaultOptions,options);
+		
+		/* each group of element */
+		 return this.each(function(){
+		
+			$('input:checkbox', this).jqTransCheckBox();
+			$('input:radio', this).jqTransRadio();
+			
+			if( $('select', this).jqTransSelect().length > 0 ){cTPAddDocumentListener();}
+			$(this).bind('reset',function(){var action = function(){cTPReset(this);}; window.setTimeout(action, 10);});
+
+		}); /* End  each */
+				
+	};/* End the Plugin */
+
+})(jQuery);
+
+//call the main function
+$(function() {$(".transpie").cTP();});				   
