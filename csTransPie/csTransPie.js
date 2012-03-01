eae74c4d3463e70a8ecbc4bae337e5584165df4c
Petar Koretic <petar.koretic@gmail.com>
2012-03-01 22:26:19 +0100

    Completly new version

--- csTransPie/csTransPie.js
+++ csTransPie/csTransPie.js
@@ -1,7 +1,6 @@
 /*
-   inspired by jqTransform http://www.dfc-e.com/metiers/multimedia/opensource/jqTransform/
-   
- * Started 30.01.2012 Petar Koretić
+  
+ * Petar Koretić
  ******************************************** */
  
  //Load css dynamically - depends on folder name - csTransPie by default
@@ -18,26 +17,10 @@ else {
  */
 
 (function($){
-
-	/* Hide all open selects */
-	var cTPHideSelect = function(oTarget){
-		var ulVisible = $('.cTPSelectWrapper ul:visible');
-		ulVisible.each(function(){
-			var oSelect = $(this).parents(".cTPSelectWrapper:first").find("select").get(0);
-			//do not hide if click on the label object associated to the select
-			if( !(oTarget && oSelect.oLabel && oSelect.oLabel.get(0) == oTarget.get(0)) ){$(this).hide();}
-		});
-	};
-	/* Check for an external click */
-	var cTPCheckExternalClick = function(event) {
-		if ($(event.target).parents('.cTPSelectWrapper').length === 0) { cTPHideSelect($(event.target)); }
-	};
-
-	/* Apply document listener */
-	var cTPAddDocumentListener = function (){
-		$(document).mousedown(cTPCheckExternalClick);
-	};	
-			
+	
+/**============================================================================================================================================
+															RESET FORM FUNCTION
+==============================================================================================================================================*/			
 	/* Reset each element on previous state - it obviously has a point only if there is a form */
 	var cTPReset = function(f){
 		var sel=0; //which select in a form if there are more
@@ -48,7 +31,7 @@ else {
 		});
 		
 		//set checkbox to original value we saved
-		$('input:checkbox', f).each(function()
+		$(':checkbox', f).each(function()
 		{	
 			//if it was checked set class of checked, else, set it unchecked
 			$(this).data('val') && $('a', $(this).parent()).addClass('cTPChecked') || $('a', $(this).parent()).removeClass('cTPChecked');
@@ -56,252 +39,223 @@ else {
 		});//	$('input:checkbox', f).each(function()
 		
 		//set radio to original value we saved
-		$('input:radio', f).each(function()
+		$(':radio', f).each(function()
 		{
 			//if it was checked set class of checked, else, set it unchecked
 			$(this).data('val') && $('a', $(this).parent()).addClass('cTPCheckedR') || $('a', $(this).parent()).removeClass('cTPCheckedR');
 		});//$('input:radio', f).each(function()
 		
+		$(':file', f).each(function()
+		{
+			//for security reasons we can't set value for input file in many browsers so we do this...
+			$(this).val("");//if browser can set value (older browsers) set it to none
+			$(".cTPFileInput",$(this).parent()).html("&nbsp;"); //set our element to none
+		});//$('input:radio', f).each(function()
+		
 	}; //var cTPReset = function(f){
 
-	/***************************
-	  Check Boxes 
-	 ***************************/
+/*=============================================================================================================================================
+																		CHECKBOX 
+==============================================================================================================================================*/
 	 
 	$.fn.cTPCheckBox = function(){
 		return this.each(function(){
-			if($(this).hasClass('cTPHidden')) {return;} //prevent multiple styles
 			var checkbox = $(this); //jquery object of original element
 
 			checkbox.data('val', this.checked); //save original value
+			var aElem = $('<a href="#" class="cTPCheckboxElem"></a>'); //create our new element
 			
-			var aLink = $('<a href="#" class="cTPCheckbox"></a>'); //create our new element
-			
-			checkbox.addClass('cTPHidden').wrap('<span class="cTPCheckboxWrapper"></span>').parent().prepend(aLink); //hide original element and create our new element
+			//hide original element and create our new element
+			checkbox.wrap('<span class="cTPCheckbox"></span>').parent().prepend(aElem); 
 			
 			// set the default state - if checked, set checked class or leave it
-			this.checked && aLink.addClass('cTPChecked');
-			
-			//style label if bound to our element
-			$("label[for="+checkbox.attr('id')+"]").css("cursor","pointer"); //cursor pointer for our label
-			/*============================================ Events ====================================================*/
-			
-			//if we click on label all browsers except IE will work for hidden elements so we prevent it, and set it manually
-			$("label[for="+checkbox.attr('id')+"]").on("click",function()
-			{
-				checkbox.click(); // 'click' by all browsers
-				return false; //prevent default action which is click since we clicked manually, e.preventDefault()
-			});
-			
-			// click on our new element just triggers click on original element - browser will do the rest
-			aLink.on("click",function()
-			{
-				checkbox.click();	// 'click' by all browsers 	
-				return false; //prevent click - which sets # in url
-			});
+			this.checked && aElem.addClass('cTPChecked');
 			
 			//if original checkbox clicked change the class of our element, browser will change the state of real element
-			checkbox.on("click",function()
+			checkbox.on("change",function()
 			{
-				if(checkbox.attr('disabled'))return false; //do nothing if the original input is disabled
-				
 				//if checkbox isn't checked, add checked class, else remove checked class
-				!this.checked && aLink.addClass('cTPChecked') || aLink.removeClass('cTPChecked');
+				this.checked && aElem.addClass('cTPChecked') || aElem.removeClass('cTPChecked');
 			});
-			/*============================================ End of Events =============================================*/
-				
+	
 		});
 	};
 	
-	/***************************
-	  Title
-	 ***************************/
-	
-	$.fn.cTPTitle = function(){
-	
-		return this.each(function(){
-			title=$(this);
-			
-		    var pos = title.offset();
-			var width = title.outerWidth();
-
-			var text=title.attr("title");
-			
-			//remove original title (so the browser doesn't show it) and add ours - span after
-			title.attr("title","").after('<span class="cTPTitle">'+text+'</span>');
-			
-			//hover over element, I like it to be instant!
-			title.hover
-			(
-				function () 
-				{
-					title.next().css
-					({
-						top: pos.top + "px",
-						left: (pos.left + width)  + "px",
-						display:"inline"
-					});
-				}, 
-				function () 
-				{
-					title.next().css("display","none");
-				}
-			);
-			
-	
-			
-		 
-		})//each
-	}//function
-	/***************************
-	  Radio Buttons 
-	 ***************************/
+/*=============================================================================================================================================
+																		RADIO 
+==============================================================================================================================================*/
 	 
 	$.fn.cTPRadio = function(){
 	
 		return this.each(function(){
-			if($(this).hasClass('cTPHidden')) {return;}
-		
+
 			var radio = $(this); //jquery object of original element
 			
 			radio.data('val', this.checked); //save original value
 			
-			var aLink = $('<a href="#" class="cTPRadio" rel="'+ this.name +'"></a>'); //create our new element
-		
-			radio.addClass('cTPHidden').wrap('<span class="cTPRadioWrapper"></span>').parent().prepend(aLink); //hide original element and create our new element
+			var aElem = $('<a href="#" class="cTPRadioElem"></a>'); //create our new element
+
+			radio.wrap('<span class="cTPRadio"></span>').parent().prepend(aElem); //hide original element and create our new element over it
 			
 			// set class 'checked' if input set as checked - <input type="radio" checked="checked" />
-			this.checked && aLink.addClass('cTPCheckedR');
-			
-			//style label if bound
-			$("label[for="+radio.attr('id')+"]").css("cursor","pointer");
-			
-			/*============================================ Events ====================================================*/
-			
-			//if we click on label all browsers except IE will work for hidden elements so we prevent it, and set it manually
-			$("label[for="+radio.attr('id')+"]").on("click",function()
-			{
-				radio.click(); // 'click' by all browsers
-				return false; //prevent default action which is click since we clicked manually, e.preventDefault()
-			});
-			
-			// click on our new element just triggers click on original element - browser will do the rest
-			aLink.on("click",function(){
-				radio.click();			
-				return false; //prevent click - which sets # in url
-			});
-			
+			this.checked && aElem.addClass('cTPCheckedR');
+
 			//if we click on radio button set class as clicked! (browser will actually click it and 'unclick' others!)
 			
-			radio.on("click",function()
+			radio.on("change",function()
 			{
-				if(radio.attr('disabled')) return false; //prevent click if disabled!
-				!this.checked && aLink.addClass('cTPCheckedR'); //no need to set class on every click
+				aElem.addClass('cTPCheckedR'); //no need to set class on every click
 				// remove checked class from all others of the same name (browser won't remove OUR class obviously)
 				$('input[name="'+radio.attr('name')+'"]',this.form).not(radio).each(function()
 					{
 						$(this).attr('type')=='radio' && $(this).prev().removeClass('cTPCheckedR'); //our elements is always before original element
 					});
 			});
-			/*============================================ End of Events =============================================*/
+			
 		});
 	};
 	
-	/***************************
-	  Select 
-	 ***************************/	
-	 
-	$.fn.cTPSelect = function(){
-			var sel=0; //if there are more selects in form we want to know which select is this in that form
-		//do this for every select
+/*=============================================================================================================================================
+																		FILE 
+==============================================================================================================================================*/
+$.fn.cTPFile = function(){
 		return this.each(function(index){
 		
-			var $select = $(this);
-			if($select.hasClass('cTPHidden')) {return;} //if it's hidden
-			if($select.attr('multiple')) {return;} //if it's a type of multiple select
-
-			/* First thing we do is Wrap it */
-			var $wrapper = $select.addClass('cTPHidden').wrap('<div class="cTPSelectWrapper select'+ sel++ +'"></div>').parent().css({zIndex: 100-index});
-			/* Now add the html for the select */
-			$wrapper.prepend('<div><span></span><a href="#" class="cTPSelectOpen">&#9660;</a></div><ul></ul>');
-			var $ul = $('ul', $wrapper).css('width',$select.width()).hide();
-			/* Now we add the options */
-			$('option', this).each(function(i){
-				var oLi = $('<li><a href="#" index="'+ i +'">'+ $(this).html() +'</a></li>');
-				$ul.append(oLi);
-			});
-			
-			/* Add click handler to the a */
-			$ul.find('a').on("click",function(){
-					$('a.selected', $wrapper).removeClass('selected');
-					$(this).addClass('selected');	
-					/* Fire the onchange event */
-					if ($select[0].selectedIndex != $(this).attr('index')) 
-					{
-						$select[0].selectedIndex = $(this).attr('index');
-						$($select[0]).trigger('change');
-					}
-					$select[0].selectedIndex = $(this).attr('index');
-					$('span:eq(0)', $wrapper).html($(this).html());
-					$ul.hide();
-					return false;
-			});
-			/* Set the default */
-			$select.data('index', this.selectedIndex); //save original value
-			$('a:eq('+ this.selectedIndex +')', $ul).click();
-			$('span:first', $wrapper).on("click",function(){$("a.cTPSelectOpen",$wrapper).click();});
-			
-			//click on label bound to select gives only focus, on regular elements, but there is no point to it here
-			/*$("label[for="+$select.attr('id')+"]").on("click",function()
-			{
-				$("a.cTPSelectOpen",$wrapper).focus(); //click on select
-				return false; //prevent default action since we did it manually
-			});*/
-			
-			/* Apply the click handler to the Open */
-			var oLinkOpen = $('a.cTPSelectOpen', $wrapper)
-				.on("click",function(){
-					//Check if box is already open to still allow toggle, but close all other selects
-					if( $ul.css('display') == 'none' ) {cTPHideSelect();} 
-					if($select.attr('disabled')){return false;}
-
-					$ul.slideToggle('fast', function(){					
-						var offSet = ($('a.selected', $ul).offset().top - $ul.offset().top);
-						$ul.animate({scrollTop: offSet});
-					});
-					return false;
-				});
-
-			// Set the new width
-			var iSelectWidth = $select.outerWidth()+20;
-			var oSpan = $('span:first',$wrapper);
-			var newWidth = (iSelectWidth > oSpan.innerWidth())?iSelectWidth+oLinkOpen.outerWidth():$wrapper.width();
-			$wrapper.css('width',newWidth);
-			$ul.css('width',newWidth);
-			oSpan.css({width:iSelectWidth});
+		file=$(this);
 		
-			// Calculate the height if necessary, less elements that the default height
-			// show the ul to calculate the block, if ul is not displayed li height value is 0
-			$ul.css({display:'block',visibility:'hidden'});
-			var iSelectHeight = ($('li',$ul).length)*($('li:first',$ul).height());//+1 else bug ff
-			(iSelectHeight < $ul.height()) && $ul.css({height:iSelectHeight,'overflow':'hidden'});//hidden else bug with ff
-			$ul.css({display:'none',visibility:'visible'});
+		if(file.hasClass('cTPHiddenFile')) {return;} //if it's hidden already
+		
+		var title="";
+		if(file.attr("title")) 
+		{
+			title='title="'+file.attr("title")+'"';
+			file.removeAttr("title");
+		}
+		var buttonText="Browse"; //default input file button value
+		if(file.data('button'))buttonText=file.data('button'); //if there is text for our button set, use that instead
+		file.addClass('cTPHiddenFile').wrap('<span '+title+' class="cTPFile"></span>').parent().prepend('<input disabled="disabled" type="text" class="cTPFileInput" /></span><input type="button" value="'+buttonText+'" />'); //hide original element and create our new element
+		
+		if(file.attr('disabled')) file.parent().find("input").addClass("cTPDisabled");
+		
+		// only click on our new button triggers click on original element - "input" field can be used for copying text from it
+		$(file.parent()).on("click","input[type=button]",function()
+		{
+			$("input[type=file]",$(this).parent()).click();
+			return false; //prevent click - which sets # in url
+		});
+		
+		//only prevent click if the original input is disabled
+		file.on("click",function()
+		{
+			if($(this).attr('disabled')) return false; 
+		});
+		
+		//on change set filename to original element filename
+		file.on("change",function()
+		{
+			$(".cTPFileInput",$(this).parent()).val($(this).val().split('\\').pop());
+		});
+
+		
+	});//function each
+};	//file function
+/*=============================================================================================================================================
+																		SELECT 
+==============================================================================================================================================*/	
+	 
+	$.fn.cTPSelect = function(){
+		//do this for every select
+		return this.each(function(){
+		
+			var select = $(this);
+			if(select.hasClass('cTPHidden')) {return;} //if it's hidden
+			if(select.attr('multiple')) {return;} //if it's a type of multiple select
 			
+			select.wrap('<div class="cTPSelect"></div>').parent().prepend('<span class="cTPSelectBar">'+$("option:selected",select).text()+'</span><a href="#" class="cTPSelectOpen">&#9660;</a>');
+			
+			if(select.attr('disabled')) $wrapper.addClass("cTPDisabled");
+			
+			select.on("change",function()
+			{
+				//update text
+				$("span:first",$(this).parent()).text($("option:selected",$(this).parent()).val());
+			});
 		});
 	};
 
-	//style every element with appropriate class
+/*=============================================================================================================================================
+																		TITLE 
+==============================================================================================================================================*/
+	
+	$.fn.cTPTitle = function(){
+	
+		return this.each(function(){
+			title=$(this);
+			//prevent multiple styles, you newer know
+			if($(this).next().hasClass('cTPTitle')) {return;} 
+			
+			//one day when we won't have to support IE<=7
+			//title.attr("data-title",title.attr("title"));
+			//title.attr("title","");
+
+			// add ours title - span after; and remove original title (so the browser doesn't show it)
+			title.after('<span class="cTPTitle">'+title.attr("title")+'</span>');
+			title.removeAttr("title");
+			
+			//position always relative to the document
+			var pos = title.position();
+			var width = title.outerWidth();
+
+			//hover over element, I like it to be instant!
+			title.hover
+			(
+				function () 
+				{
+					$(this).next().css
+					({
+						top: pos.bottom + "px",
+						left: (pos.left + width)  + 10 +"px",
+						display:"inline"
+					});
+				}, 
+				function () 
+				{
+					$(this).next().css("display","none");
+				}
+			);
+		})//each
+	}//function
+/*=============================================================================================================================================
+																		LABELS 
+==============================================================================================================================================*/
+
+	$.fn.cTPLabel = function(){
+		return this.each(function(){
+			label=$(this);
+			if(label.data("styled")) return; //if we already processed this label return
+			//if element corresponding to this label is disabled..."disable" this label (browser will disabled it, we add class
+			if($("#"+label.attr("for")).attr("disabled")=="disabled") 
+			{
+				label.addClass("cTPDisabled");
+			}
+			label.data("styled","1");//finished
+		})
+}		
+/*=============================================================================================================================================
+																MAIN FUNCTION 
+==============================================================================================================================================*/
 	$.fn.cTP = function()
 	{
-			$('.transpie input:checkbox, input:checkbox.transpie',$(this).parent()).cTPCheckBox();
-			$('.transpie input:radio, input:radio.transpie',$(this).parent()).cTPRadio();
-			$('.transpie [title], [title].transpie',$(this).parent()).cTPTitle();
 			
-			if( $('.transpie select, select.transpie',$(this).parent()).cTPSelect().length > 0 ){cTPAddDocumentListener();}
+			$(':checkbox',this).cTPCheckBox();
+			$(':radio',this).cTPRadio();
+			$(':file',this).cTPFile();
+			$('select',this).cTPSelect();			
+			$("label[for]",this).cTPLabel(); //process only labels bound to element
+		
+			$('form',this).on('reset',function(){ cTPReset(this)}); //catch reset in a form (if there is one in our element) - we have to reset our elements manually
+			$('[title]',this).cTPTitle(); //has to be last since we are adapting titles in elements before	
 			
-			//catch reset in a form (if there is one in our element) - we have to reset our elements manually
-			$('form',this).on('reset',function(){ cTPReset(this)});
-
 	};/* End the Plugin */
 
 })(jQuery);
@@ -309,5 +263,5 @@ else {
 //call the main function
 $(function() 
 {
-	$(".transpie").cTP();
+	$("body").cTP();
 });
